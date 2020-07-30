191560915089993089641692f7309adfa1c65e5d
Nicolas Mariño <nicolasmarinoparra@gmail.com>
2020-07-30 20:46:49 +0300

    add click handler and state to App

--- src/components/App.js
+++ src/components/App.js
@@ -2,18 +2,29 @@ import React from 'react';
 import '../styles/App.scss';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
-// eslint-disable-next-line no-unused-vars
 import calculate from '../logic/calculate';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      total: null, 
+      next: null, 
+      operation: null,
+    };
+  }
+
   render() {
     // eslint-disable-next-line react/jsx-one-expression-per-line
-
+    const { total, next } = this.state
+    handleClick = (buttonName) => {
+      this.setState(calculate(this.state, buttonName));
+    }; 
     return (
       <div className="calculator">
-        <Display />
-        <ButtonPanel />
+        <Display { next ? next : total} />
+        <ButtonPanel clickHandler={this.handleClick} />
       </div>
     );
   }
