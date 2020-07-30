eb3da79f282b8629e1b2ef76ebc6b11db0291138
Nicolas Mariño <nicolasmarinoparra@gmail.com>
2020-07-30 20:55:31 +0300

    add click handler to each button

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -2,37 +2,41 @@ import React from 'react';
 import Button from './Button';
 // eslint-disable-next-line react/prefer-stateless-function
 class ButtonPanel extends React.Component {
+
+  handleClick = buttonName => {
+    this.props.clickHandler(buttonName) 
+  };
   render() {
     return (
       <div className="ButtonPanel">
         <div className="row-container">
-          <Button name="AC" color="grey" />
-          <Button name="+/-" color="grey" />
-          <Button name="%" color="grey" />
-          <Button name="÷" />
+          <Button clickHandler={this.handleClick} name="AC" color="grey" />
+          <Button clickHandler={this.handleClick} name="+/-" color="grey" />
+          <Button clickHandler={this.handleClick} name="%" color="grey" />
+          <Button clickHandler={this.handleClick} name="÷" />
         </div>
         <div className="row-container">
-          <Button name="7" color="grey" />
-          <Button name="8" color="grey" />
-          <Button name="9" color="grey" />
-          <Button name="X" />
+          <Button clickHandler={this.handleClick} name="7" color="grey" />
+          <Button clickHandler={this.handleClick} name="8" color="grey" />
+          <Button clickHandler={this.handleClick} name="9" color="grey" />
+          <Button clickHandler={this.handleClick} name="X" />
         </div>
         <div className="row-container">
-          <Button name="4" color="grey" />
-          <Button name="5" color="grey" />
-          <Button name="6" color="grey" />
-          <Button name="-" />
+          <Button clickHandler={this.handleClick} name="4" color="grey" />
+          <Button clickHandler={this.handleClick} name="5" color="grey" />
+          <Button clickHandler={this.handleClick} name="6" color="grey" />
+          <Button clickHandler={this.handleClick} name="-" />
         </div>
         <div className="row-container">
-          <Button name="1" color="grey" />
-          <Button name="2" color="grey" />
-          <Button name="3" color="grey" />
-          <Button name="+" />
+          <Button clickHandler={this.handleClick} name="1" color="grey" />
+          <Button clickHandler={this.handleClick} name="2" color="grey" />
+          <Button clickHandler={this.handleClick} name="3" color="grey" />
+          <Button clickHandler={this.handleClick} name="+" />
         </div>
         <div className="row-container">
-          <Button name="0" wide color="grey" />
-          <Button name="." color="grey" />
-          <Button name="=" />
+          <Button clickHandler={this.handleClick} name="0" wide color="grey" />
+          <Button clickHandler={this.handleClick} name="." color="grey" />
+          <Button clickHandler={this.handleClick} name="=" />
         </div>
       </div>
     );
