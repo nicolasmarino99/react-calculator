20233dae5631aba4e2d7fae0e202a59918ce78be
Nicolas Mariño <nicolasmarinoparra@gmail.com>
2020-08-03 21:23:31 +0300

    change button class to functional component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,23 +1,21 @@
 /* eslint-disable react/destructuring-assignment */
 /* eslint-disable react/prefer-stateless-function */
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-class Button extends Component {
-  render() {
-    const handleClick = () => {
-      // eslint-disable-next-line react/prop-types
-      this.props.clickHandler(this.props.name);
-    };
-    const { wide, name, color } = this.props;
+const Button = props => {
+  const handleClick = () => {
+    // eslint-disable-next-line react/prop-types
+    props.clickHandler(props.name);
+  };
+  const { wide, name, color } = props;
 
-    const sizeClass = wide ? 'big-btn' : 'small-btn';
-    const colorClass = color || 'orange';
+  const sizeClass = wide ? 'big-btn' : 'small-btn';
+  const colorClass = color || 'orange';
 
-    // eslint-disable-next-line react/button-has-type
-    return (<button className={`Button ${sizeClass} ${colorClass}`} onClick={handleClick}>{name}</button>);
-  }
-}
+  // eslint-disable-next-line react/button-has-type
+  return (<button className={`Button ${sizeClass} ${colorClass}`} onClick={handleClick}>{name}</button>);
+};
 
 Button.propTypes = {
   // eslint-disable-next-line react/require-default-props
